d55af043b0d7cb61c87aa7ae27c1e2f08ef09667
Thomas FARRE <tfarre@sutunam.com>
2024-05-30 10:44:52 +0200

    remove game page only for one user

--- src/router/index.js
+++ src/router/index.js
@@ -25,21 +25,15 @@ const router = createRouter({
 
 const auth = getAuth();
 
+
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
   onAuthStateChanged(auth, (user) => {
     if (requiresAuth && !user) {
       // If user tries to access a guarded route without being logged in, redirect to home to trigger login
       next("/");
-    } else if (requiresAuth && user) {
-      // Verify the user ID in the route matches the logged-in user's ID
-      if (to.params.userId !== user.uid) {
-        next(false); // Stop the navigation if the IDs do not match
-      } else {
-        next(); // Proceed if the IDs match
-      }
     } else {
-      next(); // Proceed normally for non-guarded routes
+      next(); // Proceed normally for all routes
     }
   });
 });
