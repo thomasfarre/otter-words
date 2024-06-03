a2f119bfa34498c383c4cfc265555527aa0d2e63
Thomas FARRE <tfarre@sutunam.com>
2024-06-03 15:47:08 +0200

    small fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue}"],
+  content: ["./**/*.{html,js,vue}"],
   theme: {
     extend: {
       fontFamily: {
